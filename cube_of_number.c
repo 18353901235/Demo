e919a70165da1ab4ac400767c5e3ac92bb737dfe
kingsley <6433558@qq.com>
2018-07-23 11:33:31 +0800

    练习使用

---
+++ cube_of_number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int number;
+	printf("enter a number:");
+	scanf("%d",&number);
+	printf("cube of number is:%d \n",number*number*number);
+	getch();
+}
