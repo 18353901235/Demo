6afd914e16dee38f18ce50258c1c88e824833fdf
kingsley <6433558@qq.com>
2018-07-31 16:00:38 +0800

    新增java

--- Hello.c
+++ Hello.c
@@ -1,6 +1,27 @@
 # include <stdio.h>
-int main()
-{
-    printf("%s\n","hello world");
-    return 0;//
+
+void function1(){
+	int a=10;
+	static int b=10;
+	a=a+1;
+	b=b+1;
+	printf("%d\n",a );
+	printf("%d\n", b);
 }
+
+void printPoint(){
+	int a=90;
+	printf("%d\n   %u\n",a,&a );
+}
+
+void main()
+{
+
+	printPoint();
+    printf("%s\n","hello world");
+    function1();
+    function1();
+    function1();
+    function1();
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-这是我的联系demo。。
-学习c/c++,php,python等
